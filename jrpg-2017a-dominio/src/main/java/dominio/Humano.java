6be7f0cab23e5812190347f646261769164f5613
Pablo Ariel Iglesias <pabloarieliglesias@gmail.com>
2017-04-13 23:53:46 -0300

    the first one

---
+++ jrpg-2017a-dominio/src/main/java/dominio/Humano.java
@@ -0,0 +1,45 @@
+package dominio;
+
+public class Humano extends Personaje {
+
+	public Humano(String nombre, Casta casta, int id) {
+		super(nombre, casta, id);
+		saludTope += 5;
+		energiaTope += 5;
+		salud = saludTope;
+		energia = energiaTope;
+		nombreRaza = "Humano";
+	}
+
+	public Humano(String nombre, int salud, int energia, int fuerza, int destreza, int inteligencia, Casta casta,
+			int experiencia, int nivel, int idPersonaje) {
+		super(nombre, salud, energia, fuerza, destreza, inteligencia, casta, experiencia, nivel, idPersonaje);
+		nombreRaza = "Humano";
+
+		habilidadesRaza = new String[2];
+		habilidadesRaza[0] = "Incentivar";
+		habilidadesRaza[1] = "Golpe Fatal";
+	}
+
+	// Incentivar
+	public boolean habilidadRaza1(Peleable atacado) {
+		if (this.getEnergia() > 10) {
+			this.setEnergia(this.getEnergia() - 10);
+			atacado.setAtaque(atacado.getAtaque() + this.getMagia());
+			return true;
+		}
+		return false;
+	}
+
+	// Golpe Fatal
+	public boolean habilidadRaza2(Peleable atacado) {
+		if (this.getEnergia() > 10) {
+			if (atacado.serAtacado(atacado.getSalud() / 2) > 0) {
+				this.setEnergia(this.getEnergia() / 2);
+				return true;
+			}
+		}
+		this.setEnergia(this.getEnergia() - 10);
+		return false;
+	}
+}
