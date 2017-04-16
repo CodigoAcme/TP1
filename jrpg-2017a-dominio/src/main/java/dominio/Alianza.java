8cd9966e1397a9c88a62dcc467e0464de2bebc2c
Ruben <mandrakelinux07@gmail.com>
2017-04-16 15:14:16 -0300

    Hechos los Javadoc de las clases Alianza, Asesino, Elfo, Guerrero y Hechicero.

--- jrpg-2017a-dominio/src/main/java/dominio/Alianza.java
+++ jrpg-2017a-dominio/src/main/java/dominio/Alianza.java
@@ -1,7 +1,9 @@
 package dominio;
 
 import java.util.LinkedList;
-
+/**Clase Alianza. Se ocupa de formar un lista de aliados y 
+ * se le asigna un nombre representavo
+ */
 public class Alianza {
 
 	String nombre; df
@@ -23,11 +25,15 @@ public class Alianza {
 	public String obtenerNombre(){
 		return nombre;
 	}
-	
+	/**Método eliminarPersonaje.
+	 * @param pj es el personaje que eliminaremos de nuestra Alianza.
+	 */
 	public void eliminarPersonaje(Personaje pj){
 		aliados.remove(pj);
 	}
-	
+	/**Método añadirPersonaje.
+	 * @param pj es el personaje que se unirá a una Alianza.
+	 */
 	public void aÃ±adirPersonaje(Personaje pj){
 		aliados.add(pj);
 	}
