0a56038378963f9c2cc6d00397aced06402519ad
cerebroiglesias <Iglesias@Iglesias-PC>
2017-04-16 15:28:25 -0300

    Casta tiene una breve descripcion

--- jrpg-2017a-dominio/src/main/java/dominio/Alianza.java
+++ jrpg-2017a-dominio/src/main/java/dominio/Alianza.java
@@ -1,7 +1,9 @@
 package dominio;
 
 import java.util.LinkedList;
-
+/**Clase Alianza. Se ocupa de formar un lista de aliados y 
+ * se le asigna un nombre representavo
+ */
 public class Alianza {
 
 	String nombre; df
@@ -23,11 +25,15 @@ public class Alianza {
 	public String obtenerNombre(){
 		return nombre;
 	}
-	
+	/**M�todo eliminarPersonaje.
+	 * @param pj es el personaje que eliminaremos de nuestra Alianza.
+	 */
 	public void eliminarPersonaje(Personaje pj){
 		aliados.remove(pj);
 	}
-	
+	/**M�todo a�adirPersonaje.
+	 * @param pj es el personaje que se unir� a una Alianza.
+	 */
 	public void añadirPersonaje(Personaje pj){
 		aliados.add(pj);
 	}
